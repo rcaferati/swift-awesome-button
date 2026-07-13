8a4b93b9c5aac16f9674a89d3a133e4b4d3ba694
rcaferati <rafael@caferati.dev>
2026-07-13 20:57:34 -0300

    Add current Swift button workspace artifacts

---
+++ build/IOSAwesomeButtonDemoAppDerivedData/Build/Intermediates.noindex/IOSAwesomeButtonDemoApp.build/Debug-iphonesimulator/IOSAwesomeButtonDemoApp.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,33 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "facebook-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameFacebookLogoFill AC_SWIFT_PRIVATE = @"facebook-logo-fill";
+
+/// The "instagram-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameInstagramLogoFill AC_SWIFT_PRIVATE = @"instagram-logo-fill";
+
+/// The "linkedin-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameLinkedinLogoFill AC_SWIFT_PRIVATE = @"linkedin-logo-fill";
+
+/// The "messenger-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameMessengerLogoFill AC_SWIFT_PRIVATE = @"messenger-logo-fill";
+
+/// The "pinterest-logo-fill" asset catalog image resource.
+static NSString * const ACImageNamePinterestLogoFill AC_SWIFT_PRIVATE = @"pinterest-logo-fill";
+
+/// The "whatsapp-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameWhatsappLogoFill AC_SWIFT_PRIVATE = @"whatsapp-logo-fill";
+
+/// The "x-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameXLogoFill AC_SWIFT_PRIVATE = @"x-logo-fill";
+
+/// The "youtube-logo-fill" asset catalog image resource.
+static NSString * const ACImageNameYoutubeLogoFill AC_SWIFT_PRIVATE = @"youtube-logo-fill";
+
+#undef AC_SWIFT_PRIVATE
